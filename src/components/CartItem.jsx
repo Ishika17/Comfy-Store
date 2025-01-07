3d999e7a63e7f6299e12aab08c29d3b3bd60a1e8
Ishika Goyal <84057985+Ishika17@users.noreply.github.com>
2025-01-07 21:04:48 +0530

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -19,21 +19,21 @@ const CartItem = ({ cartItem }) => {
       key={cartID}
       className='mb-12 flex flex-col gap-y-4 sm:flex-row flex-wrap border-b border-base-300 pb-6 last:border-b-0'
     >
-      {/* IMAGE */}
+
       <img
         src={image}
         alt={title}
         className='h-24 w-24 rounded-lg sm:h-32 sm:w-32 object-cover'
       />
-      {/* INFO */}
+  
       <div className='sm:ml-16 sm:w-48'>
-        {/* TITLE */}
+     
         <h3 className='capitalize font-medium'>{title}</h3>
-        {/* COMPANY */}
+      
         <h4 className='mt-2 capitalize text-sm text-neutral-content'>
           {company}
         </h4>
-        {/* COLOR */}
+    
         <p className='mt-4 text-sm capitalize flex items-center gap-x-2'>
           color :
           <span
@@ -43,7 +43,7 @@ const CartItem = ({ cartItem }) => {
         </p>
       </div>
       <div className='sm:ml-12'>
-        {/* AMOUNT */}
+     
         <div className='form-control max-w-xs'>
           <label htmlFor='amount' className='label p-0'>
             <span className='label-text'>Amount</span>
@@ -67,7 +67,7 @@ const CartItem = ({ cartItem }) => {
         </button>
       </div>
 
-      {/* PRICE */}
+    
       <p className='font-medium sm:ml-auto'>{formatPrice(price)}</p>
     </article>
   );
