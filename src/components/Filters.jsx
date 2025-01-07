c229b3fef8a0c44ce5b858de0edb44c590ca9ef4
Ishika Goyal <84057985+Ishika17@users.noreply.github.com>
2025-01-07 21:07:33 +0530

    Update Filters.jsx

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -9,7 +9,7 @@ const Filters = () => {
 
   return (
     <Form className='bg-base-200 rounded-md px-8 py-4 grid gap-x-4  gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
-      {/* SEARCH */}
+    
       <FormInput
         type='search'
         label='search product'
@@ -17,7 +17,7 @@ const Filters = () => {
         size='input-sm'
         defaultValue={search}
       />
-      {/* CATEGORIES */}
+    
       <FormSelect
         label='select category'
         name='category'
@@ -25,7 +25,7 @@ const Filters = () => {
         size='select-sm'
         defaultValue={category}
       />
-      {/* COMPANIES */}
+    
       <FormSelect
         label='select company'
         name='company'
@@ -33,7 +33,7 @@ const Filters = () => {
         size='select-sm'
         defaultValue={company}
       />
-      {/* ORDER */}
+      
       <FormSelect
         label='sort by'
         name='order'
@@ -41,21 +41,21 @@ const Filters = () => {
         size='select-sm'
         defaultValue={order}
       />
-      {/* PRICE */}
+   
       <FormRange
         name='price'
         label='select price'
         size='range-sm'
         price={price}
       />
-      {/* SHIPPING */}
+    
       <FormCheckbox
         name='shipping'
         label='free shipping'
         size='checkbox-sm'
         defaultValue={shipping}
       />
-      {/* BUTTONS */}
+     
       <button type='submit' className='btn btn-primary btn-sm'>
         search
       </button>
