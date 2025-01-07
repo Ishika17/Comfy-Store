f1f90f668af34b5510d344e073bec92302f2c3da
Ishika Goyal <84057985+Ishika17@users.noreply.github.com>
2025-01-07 21:11:21 +0530

    Update SingleProduct.jsx

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -63,15 +63,15 @@ const SingleProduct = () => {
           </li>
         </ul>
       </div>
-      {/* PRODUCT */}
+
       <div className='mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16'>
-        {/* IMAGE */}
+       
         <img
           src={image}
           alt={title}
           className='w-96 h-96 object-cover rounded-lg lg:w-full'
         />
-        {/* PRODUCT */}
+       
         <div>
           <h1 className='capitalize text-3xl font-bold'>{title}</h1>
           <h4 className='text-xl text-neutral-content font-bold mt-2'>
@@ -79,7 +79,7 @@ const SingleProduct = () => {
           </h4>
           <p className='mt-3 text-xl'>{dollarsAmount}</p>
           <p className='mt-6 leading-8'>{description}</p>
-          {/* COLORS */}
+        
           <div className='mt-6'>
             <h4 className='text-md font-medium tracking-wider capitalize'>
               colors
@@ -100,7 +100,7 @@ const SingleProduct = () => {
               })}
             </div>
           </div>
-          {/* AMOUNT */}
+        
           <div className='form-control w-full max-w-xs'>
             <label className='label' htmlFor='amount'>
               <h4 className='text-md font-medium -tracking-wider capitalize'>
@@ -116,7 +116,7 @@ const SingleProduct = () => {
               {generateAmountOptions(20)}
             </select>
           </div>
-          {/* CART BTN */}
+        
           <div className='mt-10'>
             <button className='btn btn-secondary btn-md' onClick={addToCart}>
               Add to bag
